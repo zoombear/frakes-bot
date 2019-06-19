9dc348dc92ab0c38b59854e910dd92f079460058
Vanessa Archambault <varch02@gmail.com>
2019-06-19 15:53:08 -0700

    fix port again

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-app.listen(3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
